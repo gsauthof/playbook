c065f6d8139ee42ea8f8007c0f283a23028df1f7
Georg Sauthoff <mail@georg.so>
2018-03-17 16:33:52 +0100

    fix interactive password retry logic

--- fedora/workstation/configure.py
+++ fedora/workstation/configure.py
@@ -808,7 +808,8 @@ def get_password_interactively():
     if pw2 != pw:
       log.error("Passwords don't match")
       pw = None
-    break
+    else:
+      break
   if not pw:
     raise RuntimeError("Obtaining password interactively, failed")
   return pw
